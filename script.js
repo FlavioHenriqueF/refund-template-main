852daf13f8dc629134ae1411afe8210211d2cec8
Flavio Henrique <flavio3224henrique@gmail.com>
2025-09-12 20:43:39 -0300

    Formata o valor no padrao brl

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ const amount = document.getElementById("amount");
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
+  // Obtem o valor atual do input e remove os caracteres nao numericos.
   let value = amount.value.replace(/\D/g, "");
 
-  amount.value = value;
+  // Transforma o valor em centavos. (exemplo : 150/100 = 1.5 que e equivalente a R$ 1,50)
+  value = Number(value) / 100;
+
+  // Atualiza o valor do input
+  amount.value = formatCurrencyBRL(value);
 };
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrao BRL (real brasileiro)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  // Retorna o valor formatado.
+  return value;
+}
